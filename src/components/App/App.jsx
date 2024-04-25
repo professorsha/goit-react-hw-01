1a3a0f68d0b88ebce4f90fae2d3dc31934151df5
professorsha <69015177+professorsha@users.noreply.github.com>
2024-04-25 22:56:50 +0300

    update Profile

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,19 @@
-import { useState } from 'react';
-// import viteLogo from '/vite.svg';
+import {userData} from './';
+import Profile from '../Profile/Profile';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
 
+export default function App() {
+  console.log(userData);
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
     </>
   );
 }
-
-export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,29 @@
+import css from './Profile.module.css';
+
+export default function Profile({ name, tag, location, image, stats }) {
+  return (
+    <div className={css.container}>
+      <div>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
