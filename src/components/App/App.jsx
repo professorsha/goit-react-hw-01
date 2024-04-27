35aee4de1a404c424e7b1e6c2887f5350bf80f0e
professorsha <69015177+professorsha@users.noreply.github.com>
2024-04-27 18:28:45 +0300

    end TransactionHistory

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,9 @@
 import userData from '../../userData.json';
 import friends from '../../friends.json';
+import transactions from '../../transactions.json';
 import Profile from '../Profile/Profile';
 import FriendList from '../FriendList/FriendList';
+import TransactionHistory from '../TransactionHistory/TransactionHistory';
 import './App.css';
 
 export default function App() {
@@ -15,6 +17,7 @@ export default function App() {
         stats={userData.stats}
       />
        <FriendList friends={friends} />
+       <TransactionHistory items={transactions} />
     </>
   );
 }
